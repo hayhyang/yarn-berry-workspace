7c9ab9f3adced55f7e955cffbe4550107b95a0f0
hayhyang <hayhyang@gmail.com>
2023-09-08 17:31:51 +0900

    [packages/ui] feat: Organisms, Templates 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,4 @@
 export { default as Atoms } from "Atoms";
 export { default as MoleCules } from "Molecules";
+export { default as Organisms } from "Organisms";
+export { default as Templates } from "Templates";
