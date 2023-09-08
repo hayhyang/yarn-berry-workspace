da6946f2ae909f0096a24bd36bdd20518062b848
hayhyang <hayhyang@gmail.com>
2023-09-08 14:48:44 +0900

    [package/ui] base

---
+++ packages/ui/styles/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    neutral: {
+      900: "",
+      800: "",
+      700: "",
+      600: "",
+      500: "",
+      400: "",
+      300: "",
+      200: "",
+      100: "",
+      0o0: "#ffffff",
+    },
+    primary: "#4169E1",
+    primaryLight: "rgba(65, 105, 225, 0.15)",
+    warning: "#FF4D4D",
+    warningLight: "rgba(255, 77, 77, 0.15)",
+  },
+};
